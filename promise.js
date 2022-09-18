e74af2ca4a237633b6491f2cfa35934b397a23d1
M. Sobirin <57650455+msobirin28@users.noreply.github.com>
2022-09-18 12:12:21 +0700

    penambahan-promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,13 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (perasaan) => {
+  let IXX = await promiseTheaterIXX();
+  let VGC = await promiseTheaterVGC();
+
+  const hitungemosi = IXX.filter((perasaanIXX) => perasaanIXX.hasil === perasaan).length + VGC.filter((perasaanVGC) => perasaanVGC.hasil === perasaan).length;
+  return hitungemosi;
+};
 
 module.exports = {
   promiseOutput,
